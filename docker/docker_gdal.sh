c2c3eff5abdd6539234e7b2e15ef309be742e25d
Peter Prescott <prescott.peter@gmail.com>
2021-09-16 14:03:33 +0100

    Add files to download and load data

---
+++ docker/docker_gdal.sh
@@ -0,0 +1,1 @@
+docker run -it --net=host -v $PWD/data:/home/data osgeo/gdal
---
+++ docker/run_ogr2ogr.sh
@@ -0,0 +1,1 @@
+ogr2ogr -f "PostgreSQL" PG:"host=localhost user=piprescott password=topsecret dbname=nbhd port=5432" osopenuprn_202108.gpkg
